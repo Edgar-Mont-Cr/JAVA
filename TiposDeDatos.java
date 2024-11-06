eeacba00b7f00d5a46599b09311d25285088c63e
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-05 19:22:48 -0600

    Add files via upload

---
+++ TiposDeDatos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author aspire
+ */
+public class TiposDeDatos {
+    public static void main (String[] args){
+        byte edad = 25 ;
+        short distancia = 1000 ;
+        long poblacion =78000000000000000L;
+        double pi = 3.24155826 ;
+        char letra = 'T';
+        boolean escierto = true;
+        boolean noescierto = false;
+
+        
+    }
+    
+}
