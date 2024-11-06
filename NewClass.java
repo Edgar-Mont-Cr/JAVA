7b09c7da5dd030c5949f72baec64377ce2ad3759
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-05 19:22:30 -0600

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author aspire
+ */
+public class NewClass {
+    public static void main(String[] arge){
+        System.out.println("hola mundo");
+        
+    }
+}
