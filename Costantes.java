d120a738d491b4b15ed165cd3ffac3176e97d248
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-07 11:43:50 -0600

    Add files via upload

---
+++ Costantes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aspire
+ */
+public class Costantes {
+    public static void main(String[] args){
+        int edad = 18 ;
+        edad = 19;
+        
+        int dni = 3864742;
+        dni = 2367161;
+        
+        final int DNI = 39265382;
+        
+    }
+}
