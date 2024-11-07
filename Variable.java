0aa2672e90c034081829b2a25792d06cd80ddd7b
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-06 21:06:40 -0600

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aspire
+ */
+public class Variable {
+    public static void main(String[] args){
+        String nombre = "Edgar";
+        int edad = 28;
+        float precio = 29.99f;
+        nombre = "Maria";
+        edad = 39;
+        
+    }
+    
+}
