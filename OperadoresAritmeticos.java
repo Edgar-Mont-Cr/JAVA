867e3c7801382e23cfa94d776cc6cecb00bc3dba
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-06 21:06:02 -0600

    Add files via upload

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aspire
+ */
+public class OperadoresAritmeticos {
+    public static void main(String[] args){
+        int a= 7;
+        int b= 10;
+        int c = a+b;
+        System.out.println(c);
+        
+        
+        int a1 = 5;
+        int a2 = 10;
+        int d = a1 - a2;
+        System.out.println(d);
+        
+        
+        int i= 20;
+        int v= 5;
+        int h= i * v;
+        System.out.println(h);
+        
+        int j= 20;
+        int k= 5;
+        int m= j / k;
+        System.out.println(m);
+        
+        int it= 20;
+        int va= 5;
+        int l= i % v;
+        System.out.println(l);
+        
+        
+    }
+}
