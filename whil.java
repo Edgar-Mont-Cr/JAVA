b72d9ae7763df85fde070f6509512c844f9545d0
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-18 15:13:05 -0600

    Add files via upload

---
+++ whil.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author aspire
+ */
+public class whil {
+    public static void main(String[] args){
+        int peso ;
+        int num = Integer.parseInt(JOptionPane.showInputDialog(null,"cuantas veces vas a realizar el registro"));
+        int resp= 0;
+        float estatura, imc;          
+        while (resp<num){
+             peso = Integer.parseInt(JOptionPane.showInputDialog("captura tu peso"));
+            estatura = Float.parseFloat(JOptionPane.showInputDialog("captura tu estatura con formato de metros"));
+            imc = (peso/(estatura*estatura));
+            if (imc<18.29){
+                JOptionPane.showMessageDialog(null,"peso bajo");
+            }
+            else if (imc>=18.50 && imc <= 24.99){
+                JOptionPane.showMessageDialog(null, "peso normal");
+            }
+            else if (imc>=25 && imc <=29.99){
+                JOptionPane.showMessageDialog(null,"sobrepeso");
+            }
+            else if (imc>=30 && imc<=34.99){
+                JOptionPane.showMessageDialog(null,"obesidad leve");
+            }
+            else if (imc>=35 && imc<=39.99){
+                JOptionPane.showMessageDialog(null,"obesidad media");
+            }
+            else {
+                JOptionPane.showMessageDialog(null,"obesidad morbida");
+            }            
+            resp++;            
+        }
+    }
+}
