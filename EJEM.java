69391f0826b5e13f499ff6067bccc698ffb0bc39
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-11-28 21:22:46 -0600

    Add files via upload

---
+++ EJEM.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aspire
+ */
+public class EJEM {
+    public static void main(String[] args) {
+        byte x=0;
+        do {
+            x+=2;
+            System.out.println(x);
+        } while (x<10);
+    }
+}
