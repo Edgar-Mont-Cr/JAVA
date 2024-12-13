802f711866038555032b0e9a0a487e7b903de680
Edgar-Mont-Cr <edgmoncr@gmail.com>
2024-12-12 19:03:39 -0600

    Add files via upload

--- multi.java
+++ multi.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import javax.swing.JOptionPane;
-/**
- *
- * @author aspire
- */
 public class multi {
     public static void main(String[] edgar) {
         //declaracion de variables
+        short multi[][]=new short [3][3];
         byte ta1[][]=new byte [3][3];
         byte ta2[][]=new byte [3][3];
+        byte m,y;
         
         //ingresion de datos
         for (byte i = 0; i < ta1.length; i++) {
@@ -23,7 +17,20 @@ public class multi {
             }
         }
         
+        //multiplicacion de variables
+        m=2;
+        y=2;
+        for (int l = 0; l < ta1.length; l++) {
+            for (int i = 0; i < ta1.length; i++) {
+                multi[l][i]=(short)(ta1[l][i]*ta2[m-l][y-i]);
+            }
+        }
+        
         //impresion de valores
+        System.out.print(" -------------------\t\t");
+        System.out.print(" -------------------\t\t");
+        System.out.print(" --------------------");
+        System.out.println();
         for (int i = 0; i < ta1.length; i++) {
             for (int j = 0; j < ta1.length; j++) {
                 System.out.print(" | " + ta1[i][j] + " | ");
@@ -32,15 +39,16 @@ public class multi {
             for (int j = 0; j < ta2.length; j++) {
                 System.out.print(" | " + ta2[i][j] + " | ");
             }
+            System.out.print("\t\t");
+            for (int p = 0; p < ta2.length; p++) {
+                System.out.print(" | " + multi[i][p] + " | ");
+            }
             System.out.println();
-            System.out.print("------------------\t\t\t");
-            System.out.print("------------------");
+            System.out.print(" -------------------\t\t");
+            System.out.print(" -------------------\t\t");
+            System.out.print(" ----------------------");
             System.out.println();
         }
         System.out.println();
     }
 }    
-        
-        //multiplicacion de variables
-       
-    
