a60738940463c642dfc252d3dc885012e841f86f
Edgar-Mont-Cr <edgmoncr@gmail.com>
2025-01-13 19:59:28 -0600

    Add files via upload

--- Numeros.java
+++ Numeros.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package Primeroparte;
 import javax.swing.JOptionPane;
-/**
- *
- * @author aspire
- */
 public class Numeros {
-     public static void main(String[] args){
+     public void par1(){
     int a = Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un valor"));
     int b = Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa tu segundo valor"));
     
